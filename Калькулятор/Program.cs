14ea34a427c5f7a9efde7e98688197840e3d6fd8
Артем <@DESKTOP-JUBRBNP>
2023-09-26 22:50:40 +0300

    Добавьте файлы проекта.

---
+++ Калькулятор/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double a, b;
+        char c;
+        Console.WriteLine("Введите первое число: ");
+        a = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите способ расчёта (+, -, *, /)");
+        c = Convert.ToChar(Console.ReadLine());
+        Console.WriteLine("Введите второе число: ");
+        b = Convert.ToDouble(Console.ReadLine());
+        switch (c)
+        {
+            case '+':
+                Console.WriteLine("{0}+{1}={2}", a, b, a + b);
+                break;
+            case '-':
+                Console.WriteLine("{0}-{1}={2}", a, b, a - b);
+                break;
+            case '*':
+                Console.WriteLine("{0}*{1}={2}", a, b, a * b);
+                break;
+            case '/':
+                Console.WriteLine("{0}/{1}={2}", a, b, a / b);
+                break;
+            default:
+                Console.WriteLine("Ошибка");
+                break;
+        }
+        Console.ReadLine();
+    }
+}
